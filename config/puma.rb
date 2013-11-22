1205457bc47a7920b8a23e12ec324d9050ba9d9e
arnkort fu <arnkorty@arnkorty.info>
2013-11-22 03:04:33 -0500

    change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -8,8 +8,10 @@ pidfile "#{PUMA_PIDS_DIR}/puma.pid"
 state_path "#{PUMA_PIDS_DIR}/puma.state"
 # access_log "#{APP_ROOT}/log/puma.access.log"
 # error_log "#{APP_ROOT}/log/puma.error.log"
-bind 'tcp://0.0.0.0:8977'
+bind  'unix:///tmp/life_bills.socket'
+# bind 'tcp://0.0.0.0:8977'
 daemonize true
 workers 2
 threads 4,16
+environment 'production'
 preload_app!

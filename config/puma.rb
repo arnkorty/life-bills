d172e4964b820ee559b9404a785ed7756de00c05
Arnkorty Fu <arnkorty.fu@gmail.com>
2013-11-22 10:49:53 +0800

    add puma config

---
+++ config/puma.rb
@@ -0,0 +1,10 @@
+APP_ROOT = '/var/www/life_bills/current'
+pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
+state_path "#{APP_ROOT}/tmp/pids/puma.state"
+access_log "#{APP_ROOT}/log/puma.access.log"
+error_log "#{APP_ROOT}/log/puma.error.log"
+bind 'tcp://127.0.0.1:8977'
+daemonize true
+workers 2
+threads 0,16
+preload_app!
